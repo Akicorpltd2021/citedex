7f8d7e11ff00675f5efaa34dc06c9b3e49f729d7
Ali Timbo <alitimbo.pro11@gmail.com>
2024-05-31 13:08:11 +0200

    cite d'ex présentation

--- inc/header.php
+++ inc/header.php
@@ -1,83 +1,88 @@
-<div class="em40_header_area_main">
-  <div class="charido-header-top">
-    <div class="container ">
-      <div class="row h_top_22">
-        <div class="col-xs-12 col-md-4 col-lg-6 col-sm-12 topsr ossos">
-          <div class="top-welcomet">
-            <p class="text-left text_s_center">
-              <span>Entraide - Transparence - Expression</span>
-            </p>
+<div class="computertemplateheader">
+
+  <div class="em40_header_area_main">
+    <div class="charido-header-top">
+      <div class="container ">
+        <div class="row h_top_22">
+          <div class="col-xs-12 col-md-4 col-lg-6 col-sm-12 topsr ossos">
+            <div class="top-welcomet">
+              <p class="text-left text_s_center">
+                <span>Entraide - Transparence - Expression</span>
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="col-xs-12 col-md-8 col-lg-6 col-sm-12">
-          <div class="top-address  text-right text_s_center topmenu_1s2">
-            <p>
-              <span>
-                <i class="icofont-wall-clock"></i> Lun-Ven: 9:00 à 18:00</span>
-              <a href="#">
-                <i class="icofont-google-map"></i>60 rue C. de FOUCAULD 33150 CENON</a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="charido_header_top_two top_cr_style4 top_crt_style">
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-4 col-lg-4 col-sm-12 col-md-6 tx_top_together">
-          <div class="top_crmenu_icon">
-            <ul class="top_crmenu_i_list text-left">
-              <li><a href="#"><i class="ti-facebook"></i></a></li>
-              <li><a href="#"><i class="ti-twitter"></i></a></li>
-              <li><a href="#"><i class="ti-instagram"></i></a></li>
-            </ul>
-          </div>
-          <div class="top_crl_menu margin_l30">
-          </div>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-sm-12 d_md_none d_lg_none col-md-6">
-          <div class="theme_cr_logo text-center">
-            <div class="logo">
-              <a class="main_sticky_main_l" href="index.php" title="Cité d'Ex">
-                <img src="assets/images/Logo_simple_cite_dex.png" style="max-width: 55%;" alt="Cité d'Ex">
-              </a>
-              <a class="main_sticky_l" href="index.php" title="Cité d'Ex">
-                <img src="assets/images/Logo_simple_cite_dex.png" style="max-width: 55%;" alt="Cité d'Ex">
-              </a>
+          <div class="col-xs-12 col-md-8 col-lg-6 col-sm-12">
+            <div class="top-address  text-right text_s_center topmenu_1s2">
+              <p>
+                <span>
+                  <i class="icofont-wall-clock"></i> Lun-Ven: 9:00 à 18:00
+                </span>
+                <a href="#">
+                  <i class="icofont-google-map"></i>60 rue C. de FOUCAULD 33150 CENON
+                </a>
+              </p>
             </div>
           </div>
         </div>
-        <div class="col-xl-5 col-lg-5 col-sm-12  col-md-7">
-          <div class="top-address cr_top_addess">
-            <p class="text-right">
-              <a href="mailto:Citedex.asso@gmail.com" title="Nous contactez">
-                <i class="ti-email"></i>
-                Citedex.asso@gmail.com</a>
-            </p>
+      </div>
+    </div>
+    <div class="charido_header_top_two top_cr_style4 top_crt_style">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-4 col-lg-4 col-sm-12 col-md-6 tx_top_together">
+            <div class="top_crmenu_icon">
+              <ul class="top_crmenu_i_list text-left">
+                <li><a href="#"><i class="ti-facebook"></i></a></li>
+                <li><a href="#"><i class="ti-twitter"></i></a></li>
+                <li><a href="#"><i class="ti-instagram"></i></a></li>
+              </ul>
+            </div>
+            <div class="top_crl_menu margin_l30">
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-3 col-sm-12 d_md_none d_lg_none col-md-6">
+            <div class="theme_cr_logo text-center">
+              <div class="logo">
+                <a class="main_sticky_main_l" href="index.php" title="Cité d'Ex">
+                  <img src="assets/images/Logo_simple_cite_dex.png" style="max-width: 55%;" alt="Cité d'Ex">
+                </a>
+                <a class="main_sticky_l" href="index.php" title="Cité d'Ex">
+                  <img src="assets/images/Logo_simple_cite_dex.png" style="max-width: 55%;" alt="Cité d'Ex">
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-5 col-lg-5 col-sm-12  col-md-7">
+            <div class="top-address cr_top_addess">
+              <p class="text-right">
+                <a href="mailto:Citedex.asso@gmail.com" title="Nous contactez">
+                  <i class="ti-email"></i>
+                  Citedex.asso@gmail.com</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="charido-main-menu one_page hidden-xs hidden-sm witr_h_h22">
-    <div class="charido_nav_area scroll_fixed">
-      <div class="container">
-        <div class="row logo-left">
-          <!-- MAIN MENU -->
-          <div class="col-md-12 col-sm-12 col-xs-12 tx_menu_together">
-            <nav class="charido_menu menu_search">
-              <ul class="sub-menu">
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="#">Présentation</a></li>
-                <li><a href="#">Nos actions</a></li>
-                <li><a href="#">Nos évènements</a></li>
-                <li><a href="#">Actualités</a></li>
-                <li><a href="#">Proposition de sujet</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#" class="linkbutton">Nous soutenir</a></li>
-              </ul>
-            </nav>
+    <div class="charido-main-menu one_page hidden-xs hidden-sm witr_h_h22">
+      <div class="charido_nav_area scroll_fixed">
+        <div class="container">
+          <div class="row logo-left">
+            <!-- MAIN MENU -->
+            <div class="col-md-12 col-sm-12 col-xs-12 tx_menu_together">
+              <nav class="charido_menu menu_search">
+                <ul class="sub-menu">
+                  <li><a href="index.php">Accueil</a></li>
+                  <li><a href="#">Présentation</a></li>
+                  <li><a href="#">Nos actions</a></li>
+                  <li><a href="#">Nos évènements</a></li>
+                  <li><a href="#">Actualités</a></li>
+                  <li><a href="#">Proposition de sujet</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#" class="linkbutton">Nous soutenir</a></li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
@@ -85,79 +90,53 @@
   </div>
 </div>
 
-<div class="mobile_logo_area">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="mobilemenu_con">
-          <div class="mobile_menu_logo text-center">
-            <a href="index.html" title="charido">
-              <img src="assets/images/logo1.png" alt="charido">
-            </a>
-          </div>
-          <div class="mobile_menu_option">
-            <div class="mobile_menu_o mobile_opicon">
-              <i class="icofont-navigation-menu openclass"></i>
+<div class="mobiletcustomheader">
+  <div class="mobile_logo_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="mobilemenu_con">
+            <div class="mobile_menu_logo text-center">
+              <a href="index.php" title="Accueil">
+                <img src="assets/images/Logo_simple_cite_dex.png" alt="Cité d'Ex" class="mobilelogo">
+              </a>
             </div>
-            <div class="mobile_menu_inner mobile_p">
-              <div class="mobile_menu_content">
-                <div class="mobile_menu_logo text-center">
-                  <a href="index.html" title="charido">
-                    <img src="assets/images/logo1.png" alt="charido">
-                  </a>
-                </div>
-                <div class="menu_area mobile-menu">
-                  <nav class="charido_menu">
-                    <ul class="sub-menu">
-                      <li class="menu-item-has-children">
-                        <a href="#">Home</a>
-                        <ul class="sub-menu">
-                          <li><a href="index.html">Home Main</a></li>
-                          <li><a href="landing_page.html">Home-Landing</a></li>
-                          <li><a href="video_page.html">Home-Video</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="about.html">About</a></li>
-                      <li><a href="donation.html">Donation</a></li>
-                      <li><a href="event.html">Event</a></li>
-                      <li class="menu-item-has-children">
-                        <a href="#">Page</a>
-                        <ul class="sub-menu">
-                          <li><a href="service.html">Service</a></li>
-                          <li class="menu-item-has-children">
-                            <a href="#">Gallery</a>
-                            <ul class="sub-menu">
-                              <li><a href="gallery_2column.html">Gallery-2column</a></li>
-                              <li><a href="gallery_3column.html">Gallery-3column</a></li>
-                              <li><a href="gallery_4column.html">Gallery-4column</a></li>
-                            </ul>
-                          </li>
-                          <li><a href="testimonial.html">Testimonial</a></li>
-                          <li><a href="team.html">Team</a></li>
-                          <li><a href="faq.html">Faq’s</a></li>
-                        </ul>
-                      </li>
-                      <li class="menu-item-has-children">
-                        <a href="#">Blog</a>
-                        <ul class="sub-menu">
-                          <li><a href="blog_grid.html">Blog Grid</a></li>
-                          <li><a href="blog_left_sidebar.html">Blog Left Sidebar</a></li>
-                          <li><a href="blog_right_sidebar.html">Blog Right Sidebar</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="contact.html">Contact</a></li>
-                    </ul>
-                  </nav>
-                </div>
-                <div class="mobile_menu_o mobile_cicon">
-                  <i class="icofont-close closeclass"></i>
+            <div class="mobile_menu_option">
+              <div class="mobile_menu_o mobile_opicon">
+                <i class="icofont-navigation-menu openclass"></i>
+              </div>
+              <div class="mobile_menu_inner mobile_p">
+                <div class="mobile_menu_content">
+                  <div class="mobile_menu_logo text-center">
+                    <a href="index.html" title="charido">
+                      <img src="assets/images/Logo_simple_cite_dex.png" alt="Cite d'Ex" class="mobilelogo">
+                    </a>
+                  </div>
+                  <div class="menu_area mobile-menu">
+                    <nav class="charido_menu">
+                      <ul class="sub-menu">
+                        <li><a href="index.php">Accueil</a></li>
+                        <li><a href="#">Présentation</a></li>
+                        <li><a href="#">Nos actions</a></li>
+                        <li><a href="#">Nos évènements</a></li>
+                        <li><a href="#">Actualités</a></li>
+                        <li><a href="#">Proposition de sujet</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Nous soutenir</a></li>
+                      </ul>
+                    </nav>
+                  </div>
+                  <div class="mobile_menu_o mobile_cicon">
+                    <i class="icofont-close closeclass"></i>
+                  </div>
                 </div>
               </div>
+              <div class="mobile_overlay"></div>
             </div>
-            <div class="mobile_overlay"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
 </div>
--- index.php
+++ index.php
@@ -111,12 +111,12 @@
                 <div class="txbdsva allcostyle boxsh boxpsv txbdmb30 text-left sselect chos_img1">
                   <div class="txbdsi">
                     <div class="imageicon">
-                      <img src="assets/images/Co_choose01.png" alt="image">
+                      <img src="assets/images/engagement.png" alt="image">
                     </div>
                     <div class="txbdcon">
                       <h2 class="txbdsvtitle hlight">Engagement collaboratif</h2>
-                      <p>Lorem ipsum dolor sit amet consec
-                        elit male those in need.
+                      <p>
+                        Unissons nos forces pour un avenir meilleur grâce à l'engagement collaboratif.
                       </p>
                       <div class="txbdsvbtn txbdbtnicon">
                       </div>
@@ -133,8 +133,8 @@
                     </div>
                     <div class="txbdcon">
                       <h2 class="txbdsvtitle hlight">Innovation citoyenne</h2>
-                      <p>Lorem ipsum dolor sit amet consec
-                        elit male those in need.
+                      <p>
+                        Libérez votre potentiel et transformez votre communauté.
                       </p>
                       <div class="txbdsvbtn txbdbtnicon">
                       </div>
@@ -145,12 +145,12 @@
                 <div class="txbdsva allcostyle boxsh boxpsv txbdmb30 text-left  sselect">
                   <div class="txbdsi">
                     <div class="imageicon">
-                      <img src="assets/images/Co_choose03.png" alt="image">
+                      <img src="assets/images/ecocitoyennete.png" alt="image">
                     </div>
                     <div class="txbdcon">
                       <h2 class="txbdsvtitle hlight">Éco-citoyenneté active</h2>
-                      <p>Lorem ipsum dolor sit amet consec
-                        elit male those in need.
+                      <p>
+                        Agissez pour la planète avec une éco-citoyenneté active et responsable.
                       </p>
                       <div class="txbdsvbtn txbdbtnicon">
                       </div>
@@ -220,7 +220,7 @@
           <div class="cas_left">
             <div class="witr_couses all_couses_color couses_s1">
               <div class="witr_couses_image txbdbfall boxtexrelative txbdbfltrb100 opacitybf0">
-                <img src="assets/images/Co_couses1.jpg" alt="image">
+                <img src="assets/images/formation.jpg" alt="image">
               </div>
               <div class="witr_content_area_c">
                 <div class="witr_content_couses">
@@ -323,7 +323,7 @@
                   <a class="btnallt btnall btnallactive " href="#">Faire un don </a>
                 </div>
                 <div class="witr_couses_image txbdbfall boxtexrelative txbdbfltrb100 opacitybf0" style="margin-top: 10px;">
-                  <img src="assets/images/Co_couses3.jpg" alt="image">
+                  <img src="assets/images/culturelle.jpg" alt="image">
                 </div>
               </div>
             </div>
@@ -355,7 +355,7 @@
                   <div class="wblogthumb alltcolorstyle">
                     <div class="wblogthumbinner">
                       <a href="#">
-                        <img src="assets/images/event.jpg" alt="image">
+                        <img src="assets/images/forum.jpg" alt="image">
                       </a>
                     </div>
                   </div>
@@ -570,7 +570,7 @@
                   </div>
                   <div class="wblogthumbinner">
                     <a href="#">
-                      <img src="assets/images/blog1.jpg" alt="image">
+                      <img src="assets/images/energie.jpg" alt="image">
                     </a>
                   </div>
                 </div>
@@ -598,7 +598,7 @@
                   </div>
                   <div class="wblogthumbinner">
                     <a href="#">
-                      <img src="assets/images/blog2.jpg" alt="image">
+                      <img src="assets/images/geopolitique.jpg" alt="image">
                     </a>
                   </div>
                 </div>
@@ -626,7 +626,7 @@
                   </div>
                   <div class="wblogthumbinner">
                     <a href="#">
-                      <img src="assets/images/blog3.jpg" alt="image">
+                      <img src="assets/images/education.jpg" alt="image">
                     </a>
                   </div>
                 </div>
@@ -806,124 +806,14 @@
       </div>
     </div>
   </div>
-  <!-- project area -->
+
+  <?php
+  require_once "inc/footer.php";
+  ?>
 
 
 
-  <!-- team area -->
 
-  <!-- video area -->
-
-  <!-- testimonial area -->
-
-
-
-  <!-- footer area -->
-  <div class="witrfm_area">
-    <div class="foter_call_area">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <img src="assets/images/Logo_simple_fond bleu_cite_dex.jpg" alt="Logo">
-            <div class="tts">
-              <div class="ttin text-center">
-                <h2 class="txbdstitle sttwo hlight">Inscrivez-vous à notre newsletter</h2>
-              </div>
-            </div>
-            <form class="mc4wp-form mc4wp-form-12748" method="post" data-id="12748">
-              <div class="mc4wp-form-fields">
-                <p>
-                  <input type="email" name="email" placeholder="Adresse e-mail">
-                  <button type="submit" value="Subscribe">
-                    <span>S'inscrire</span>
-                  </button>
-                </p>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- FOOTER MIDDLE AREA -->
-    <div class="footer-middle">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="tts">
-              <div class="ttin text-left">
-                <h4 class="txbdstitle tsmall stone hlight txbdbcolor">Contact </h4>
-                <p>60 rue C. de FOUCAULD 33150 CENON</p>
-              </div>
-            </div>
-            <div class="witrlistw all_list_color">
-              <h2>Faire un don par appel</h2>
-              <ul>
-                <li>
-                  <a href="#">
-                    <i class="flaticon flaticon-phone-call"></i>
-                    +33 677 999 888</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="widget widget_nav_menu">
-              <h2 class="widget-title">Liens utiles</h2>
-              <div class="menu-categories-container">
-                <ul class="menu">
-                  <li><a href="#">Présentation</a></li>
-                  <li><a href="#">Nos actions</a></li>
-                  <li><a href="#">Nos évènements</a></li>
-                  <li><a href="#">Proposition de sujet</a></li>
-                  <li><a href="#">Contact</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="widget widget_recent_entries">
-              <h2 class="widget-title">Découvertes </h2>
-              <ul>
-                <li>
-                  <a href="#">Actualités</a>
-                </li>
-                <li>
-                  <a href="#">Devenir volontaire</a>
-                </li>
-                <li>
-                  <a href="#">Evènements à venir</a>
-                </li>
-                <li>
-                  <a href="#">Galerie photo</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="widget widget_recent_entries">
-              <h2 class="widget-title">Adresse</h2>
-              <div class="map_inner_area">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2828.039960561533!2d-0.5184814237180682!3d44.86148127345081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd552f4585a94f43%3A0xe8603a662f645573!2s60%20Rue%20Charles%20de%20Foucauld%2C%2033150%20Cenon%2C%20France!5e0!3m2!1sfr!2sne!4v1717089678773!5m2!1sfr!2sne" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <!--<iframe src="https://maps.google.com/maps?q=webitrangpur&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near" title="webitrangpur"></iframe>-->
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer-bottom">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <div class="copy-right-text text-center">
-              <p> &copy; 2024 - Cité d'Ex - Tous droits réservés.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Include All JS -->
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
   <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
